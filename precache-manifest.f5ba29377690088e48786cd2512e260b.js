4db4028bb6a67f8a3949acd9dfdf6fe3c1732d27
andypieters <andy@pay.nl>
2019-12-12 19:10:04 +0100

    deploy

---
+++ precache-manifest.f5ba29377690088e48786cd2512e260b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20ff8ccb2dec3bbcbe5",
+    "url": "/tank-app/css/chunk-vendors.226825a1.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/tank-app/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/tank-app/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/tank-app/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/tank-app/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "b6fb42ae6adda39159a1cf49b8dcddea",
+    "url": "/tank-app/index.html"
+  },
+  {
+    "revision": "95ffe87275d2a3532d6c",
+    "url": "/tank-app/js/app.167e6b6b.js"
+  },
+  {
+    "revision": "a20ff8ccb2dec3bbcbe5",
+    "url": "/tank-app/js/chunk-vendors.ab3a8ebc.js"
+  },
+  {
+    "revision": "c211eb39f6cf266627d0d0ae4ea7cc37",
+    "url": "/tank-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tank-app/robots.txt"
+  }
+]);
